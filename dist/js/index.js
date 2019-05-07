32b27e833fa3b194fd057ceeca0e0b93035a2f10
Brian Sekwakwa <briansenpai14@gmail.com>
2019-05-07 16:10:44 +0200

    added images for the skillset section

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,8 +1,12 @@
 const toggleButton = document.querySelector("#nav-icon1");
 const hiddenMenu = document.querySelector(".menu");
+const hiddenMenuLinks = document.querySelectorAll(".menu__links a");
 
 // Events
 toggleButton.addEventListener("click", openMenu);
+hiddenMenuLinks.forEach(item => {
+  item.addEventListener("click", closeOnClick);
+});
 
 // Functionality for toggle button
 $(document).ready(function() {
@@ -11,7 +15,32 @@ $(document).ready(function() {
   });
 });
 
+// Smooth Scrolling
+$("ul a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
 // Open hidden menu
 function openMenu() {
   hiddenMenu.classList.toggle("showMenu");
+}
+
+// Close hidden menu when menu links are clicked
+function closeOnClick() {
+  hiddenMenu.classList.remove("showMenu");
+  toggleButton.classList.remove("open");
 }
