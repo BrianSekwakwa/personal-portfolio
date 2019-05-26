52db1477ce1484d04c058593c21dcf6b4edd30fa
Brian Sekwakwa <briansenpai14@gmail.com>
2019-05-26 16:52:19 +0200

    Updates

---
+++ js/index.js
@@ -0,0 +1,46 @@
+const toggleButton = document.querySelector("#nav-icon1");
+const hiddenMenu = document.querySelector(".menu");
+const hiddenMenuLinks = document.querySelectorAll(".menu__links a");
+
+// Events
+toggleButton.addEventListener("click", openMenu);
+hiddenMenuLinks.forEach(item => {
+  item.addEventListener("click", closeOnClick);
+});
+
+// Functionality for toggle button
+$(document).ready(function() {
+  $("#nav-icon1").click(function() {
+    $(this).toggleClass("open");
+  });
+});
+
+// Smooth Scrolling
+$("ul a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
+// Open hidden menu
+function openMenu() {
+  hiddenMenu.classList.toggle("showMenu");
+}
+
+// Close hidden menu when menu links are clicked
+function closeOnClick() {
+  hiddenMenu.classList.remove("showMenu");
+  toggleButton.classList.remove("open");
+}
